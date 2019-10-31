347df05e6fbac41a7a26d6f9467a004ca9501c2f
Filipe Santiago <filipe@upplify.com>
2019-10-31 20:57:44 -0300

    add logger and config

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,29 @@
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+import * as helmet from 'helmet';
+import * as morgan from 'morgan';
+
+import { AppModule } from './app/app.module';
+import { AppLoggerService } from './app/logger/services/app-logger.service';
 
 async function bootstrap () {
 	const app = await NestFactory.create(AppModule);
+	app.setGlobalPrefix('api/v1');
+
+	const logger = app.get(AppLoggerService);
+
+	// middleware
+	app.use(helmet());
+	app.useLogger(logger);
+	app.use(morgan('combined', { stream: app.get('LoggerStreamProvider') }));
+
+	app.useGlobalPipes(
+		new ValidationPipe({
+			forbidUnknownValues: true,
+			validationError: { target: false, value: true }
+		})
+	);
+
 	await app.listen(3000);
 }
 bootstrap();
