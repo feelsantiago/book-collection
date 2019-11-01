97f5389598162637175c5fd601caf25faedf9468
Filipe Santiago <filipe@upplify.com>
2019-10-31 21:32:12 -0300

    add imports to auth module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,8 +2,16 @@ import { Module } from '@nestjs/common';
 import { DatabaseModule } from './database/database.module';
 import { LoggerModule } from './logger/logger.module';
 import { ConfigModule } from './config/config.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-	imports: [ DatabaseModule, LoggerModule, ConfigModule ]
+	imports: [
+		DatabaseModule,
+		LoggerModule,
+		ConfigModule,
+
+		// Api Modules
+		AuthModule
+	]
 })
 export class AppModule {}
--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -1,9 +1,29 @@
 import { Module } from '@nestjs/common';
+import { PassportModule } from '@nestjs/passport';
+import { JwtModule } from '@nestjs/jwt';
+
 import { ConfigModule } from '../config/config.module';
 import { DatabaseModule } from '../database/database.module';
 import { LoggerModule } from '../logger/logger.module';
+import { AuthService } from './auth.service';
+import { JwtStrategy } from './strategies/jwt.strategy';
+import { JwtAuthGuard } from './guards/jwt-auth.guard';
+import { RolesGuard } from './guards/roles.guard';
 
 @Module({
-	imports: [ ConfigModule, DatabaseModule, LoggerModule ]
+	imports: [
+		ConfigModule,
+		DatabaseModule,
+		LoggerModule,
+		PassportModule.register({ defaultStrategy: 'jwt' }),
+		JwtModule.register({
+			secretOrPrivateKey: 'b00k$c0Lect1on$$',
+			signOptions: {
+				// expiresIn: 3600
+			}
+		})
+	],
+	providers: [ AuthService, JwtStrategy, JwtAuthGuard, RolesGuard ],
+	exports: [ AuthService, JwtAuthGuard, RolesGuard ]
 })
 export class AuthModule {}
