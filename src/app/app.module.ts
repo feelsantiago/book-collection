8ee7a44f79f568951c689300a85afc120c8b1962
Filipe Santiago <filipe@upplify.com>
2019-10-31 21:12:01 -0300

    add modules to app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,9 @@
 import { Module } from '@nestjs/common';
+import { DatabaseModule } from './database/database.module';
+import { LoggerModule } from './logger/logger.module';
+import { ConfigModule } from './config/config.module';
 
 @Module({
-	imports: []
+	imports: [ DatabaseModule, LoggerModule, ConfigModule ]
 })
 export class AppModule {}
